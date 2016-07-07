29ee6fa08562bdd9983db2f5288b6b4f8f9cfaed
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-07-07 17:31:09 +0900

    angularfire2 works

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
+import { FIREBASE_PROVIDERS, defaultFirebase } from 'angularfire2';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  FIREBASE_PROVIDERS,
+  // Initialize Firebase app  
+  defaultFirebase({
+    apiKey: "AIzaSyAd1FggQh-cpIRcUqHg7RQPrTCj5g-EzSc",
+    authDomain: "fir-2practice.firebaseapp.com",
+    databaseURL: "https://fir-2practice.firebaseio.com",
+    storageBucket: "gs://fir-2practice.appspot.com",
+  })
+]);
 
